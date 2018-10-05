ca95d5e1a3fc26105aa9d26083c0fc1d4c348ef3
unknown <bfarkas981@gmail.com>
2018-10-05 11:16:01 +0200

    Init

---
+++ source/constans.py
@@ -0,0 +1,6 @@
+AUTHOR="Farkas Balázs / P3L47H"
+APPNAME="Panoráma kép készítő alkalmazás"
+FIGURE_TITLE=APPNAME + "\n"+AUTHOR
+#PATH="C:\\Learn\Phyton\Panorama\\test1"
+PATH="test1"
+INCLUDED_EXTENSIONS = ['JPG', 'bmp', 'png', 'gif']
---
+++ source/main.py
@@ -0,0 +1,52 @@
+#Panoráma kép készítő alkalmazás
+#Farkas Balázs P3L47H
+
+
+# import references
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
+import constans
+
+#Start
+print("Start application: ", constans.APPNAME)
+
+#get files from path
+print("Path: ",constans.PATH)
+imageNames = [fn for fn in os.listdir(constans.PATH)
+              if any(fn.upper().endswith(ext) for ext in constans.INCLUDED_EXTENSIONS)]
+print(imageNames)
+numberOfImages= len(imageNames)
+images= [None] * numberOfImages
+imageCounter=0
+
+# Load imagefiles to array
+for imageName in imageNames:
+    fullImagePath=constans.PATH+"\\"+imageName
+    images[imageCounter]=cv2.imread(fullImagePath)
+    images[imageCounter]=images[imageCounter][:,:,::-1] #BGR>>RGB
+    imageCounter+=1
+    print(imageCounter,": ",imageName)
+
+numberOfImages+=3
+fig, axes = plt.subplots(nrows = 2, ncols = numberOfImages)
+fig.suptitle(constans.FIGURE_TITLE, fontsize=16)
+
+for ax,image in zip(axes.flat,images):
+   ax.imshow(image)
+
+# remove all axes from second row
+for i in range(numberOfImages,1,-1):
+    ax=axes[1,i-1]
+    ax.remove()
+
+# add axes in full width
+gs = axes[0, 0].get_gridspec()
+axbig = fig.add_subplot(gs[1, 0:])
+
+
+# set plt to full screen and show
+mng = plt.get_current_fig_manager()
+mng.resize(*mng.window.maxsize())
+plt.show()
