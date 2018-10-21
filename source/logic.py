a9b8aaeec290aa749bb7fff0cebed8564a11aea3
unknown <bfarkas981@gmail.com>
2018-10-21 23:45:32 +0200

    Get sampling

--- source/logic.py
+++ source/logic.py
@@ -15,11 +15,11 @@ def mergeTwoImage(image1, image2):
     totalImage=np.zeros((maxD1,maxD2,3), 'uint8')
 
 
-    sampling=np.zeros((10,3), 'uint8')
+    sampling=np.zeros((5,3), 'uint8')
     samplingCounter=0
-    samplingCenter=int(round(maxD1/2))-5
-    while samplingCounter<10:
-        sampling[samplingCounter]=image1[0,samplingCenter+samplingCounter]
+    samplingCenter=int(round(maxD1/2))
+    while samplingCounter<5:
+        sampling[samplingCounter]=image1[samplingCenter,samplingCounter]
         samplingCounter+=1
     
     print("image1: ",len(image1),len(image1[0]))
@@ -28,13 +28,25 @@ def mergeTwoImage(image1, image2):
 
 
     currentD1=0
+    correctCounter=0
+    correctOffset=0
     while currentD1<len(image1):
         currentD2=0
         while currentD2<len(image1[currentD1]):
             totalImage[currentD1,currentD2]=image1[currentD1,currentD2]
+            if image1[currentD1,currentD2]==sampling[correctCounter]:
+                correctCounter+=1
+            else:
+                correctCounter=0
+            if correctCounter==5:
+                correctOffset=currentD2
             currentD2+=1
         currentD1+=1
+
+
     offsetD2=currentD2-1    # ez a középvonal is!
+    if CorrectD2!=0:
+        offsetD2=correctOffset
     currentD1=0
     while currentD1<len(image2):
         currentD2=0
